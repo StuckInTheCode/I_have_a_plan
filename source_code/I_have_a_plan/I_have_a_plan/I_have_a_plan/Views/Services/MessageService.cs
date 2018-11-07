38db918cdf99194fd5ec9f64a89782936ce73028
StuckInTheCode <gala-1999@tut.by>
2018-11-08 01:03:48 +0300

    Add messageServices

---
+++ source_code/I_have_a_plan/I_have_a_plan/I_have_a_plan/Views/Services/MessageService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using I_have_a_plan.ViewModels.Services;
+
+namespace I_have_a_plan.Views.Services
+{
+    class MessageService: IMessageService
+    {
+        public MessageService()
+        {
+        }
+
+        #region IMessageServise implementation
+        public async Task ShowAsync(string message)
+        {
+            await Xamarin.Forms.Application.Current.MainPage.DisplayAlert("Message", message, "Ok");
+        }
+        #endregion
+    }
+}
