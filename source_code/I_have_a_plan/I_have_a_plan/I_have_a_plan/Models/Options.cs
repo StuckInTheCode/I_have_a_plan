6cd4b1952a785b51baecbf8d25a39e5752ea96de
StuckInTheCode <gala-1999@tut.by>
2018-11-17 23:06:50 +0300

    Add options and save its state to the app properties

--- source_code/I_have_a_plan/I_have_a_plan/I_have_a_plan/Models/Options.cs
+++ source_code/I_have_a_plan/I_have_a_plan/I_have_a_plan/Models/Options.cs
@@ -4,7 +4,125 @@ using System.Text;
 
 namespace I_have_a_plan.Models
 {
+    public enum NotificationType
+    {
+        Sound,
+        SoundVibration,
+        Vibration,
+        NoSound
+    }
+
     public class Options
     {
+        public Int32 notificationPeriod;
+        public NotificationType notificationType;
+        public TimeSpan notificationStartTime;
+        public TimeSpan notificationEndTime;
+
+        public Options()
+        {
+            LoadOptions();
+        }
+
+        public async void SaveOptions()
+        {
+            JSONSerializer jSON = new JSONSerializer();
+            if (App.Current.Properties.Count != 0)
+            {
+                App.Current.Properties["notificationPeriod"] = notificationPeriod;
+                App.Current.Properties["notificationType"] = Newtonsoft.Json.JsonConvert.SerializeObject(notificationType);
+                App.Current.Properties["notificationStartTime"] = Newtonsoft.Json.JsonConvert.SerializeObject(notificationStartTime);
+                App.Current.Properties["notificationEndTime"] = Newtonsoft.Json.JsonConvert.SerializeObject(notificationEndTime);
+            }
+            else
+            {
+                App.Current.Properties.Add("notificationPeriod", notificationPeriod);
+                App.Current.Properties.Add("notificationType", Newtonsoft.Json.JsonConvert.SerializeObject(notificationType));
+                App.Current.Properties.Add("notificationStartTime", Newtonsoft.Json.JsonConvert.SerializeObject(notificationStartTime));
+                App.Current.Properties.Add("notificationEndTime", Newtonsoft.Json.JsonConvert.SerializeObject(notificationEndTime));
+            }
+            await App.Current.SavePropertiesAsync();
+        }
+
+        public void LoadOptions()
+        {
+            if (App.Current.Properties.TryGetValue("notificationPeriod", out object obj))
+            {
+                notificationPeriod = (int)obj;
+            }
+            else
+            {
+                notificationPeriod = 0;
+            }
+            if (App.Current.Properties.TryGetValue("notificationType", out obj))
+            {
+                notificationType = Newtonsoft.Json.JsonConvert.DeserializeObject<NotificationType>(obj.ToString());
+            }
+            else
+            {
+                notificationType = NotificationType.Sound;
+            }
+            if (App.Current.Properties.TryGetValue("notificationStartTime", out obj))
+            {
+                notificationStartTime = Newtonsoft.Json.JsonConvert.DeserializeObject<TimeSpan>(obj.ToString());
+            }
+            else
+            {
+                notificationStartTime = new TimeSpan();
+            }
+            if (App.Current.Properties.TryGetValue("notificationEndTime", out obj))
+            {
+                notificationEndTime = Newtonsoft.Json.JsonConvert.DeserializeObject<TimeSpan>(obj.ToString());
+            }
+            else
+            {
+                notificationEndTime = new TimeSpan();
+            }
+        }
+
+        public void SetNotificationType(string Time)
+        {
+            switch (Time)
+            {
+                case "Sound":
+                    notificationType = NotificationType.Sound;
+                    break;
+                case "Sound and vibration":
+                    notificationType = NotificationType.SoundVibration;
+                    break;
+                case "Vibration":
+                    notificationType = NotificationType.Vibration;
+                    break;
+                case "No sound":
+                    notificationType = NotificationType.NoSound;
+                    break;
+            }
+        }
+
+        public string GetNotificationType()
+        {
+            string type = "";
+
+            switch (notificationType)
+            {
+                case NotificationType.Sound:
+                    type = "Sound";
+                    break;
+                case NotificationType.SoundVibration:
+                    type = "Sound and vibration";
+                    break;
+                case NotificationType.Vibration:
+                    type = "Vibration";
+                    break;
+                case NotificationType.NoSound:
+                    type = "No sound";
+                    break;
+                default:
+                    type = "Vibration";
+                    break;
+
+            }
+            return type;
+        }
     }
 }
---
+++ source_code/I_have_a_plan/I_have_a_plan/I_have_a_plan/ViewModels/OptionsViewModel.cs
@@ -0,0 +1,99 @@
+﻿using I_have_a_plan.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace I_have_a_plan.ViewModels
+{
+   public class OptionsViewModel
+    {
+        private Options Options;
+
+        public OptionsViewModel()
+        {
+            Options = new Options();
+            TypeList = new List<string>();
+            PeriodList = new List<string>();
+
+            TypeList.AddRange(new string[] { "Sound", "Sound and vibration", "Vibration", "No sound" });
+
+            PeriodList.AddRange(new string[] { "1 hour", "2 hours", "3 hours",
+                "4 hours", "5 hours", "6 hours", "7 hours", "8 hours", "9 hours",
+                "10 hours", "11 hours", "12 hours", "13 hours", "14 hours", "15 hours",
+                "16 hours", "17 hours", "18 hours", "19 hours", "20 hours", "21 hours",
+                "22 hours", "23 hours", "24 hours" });
+        }
+
+        public List<string> TypeList { get; }
+
+        public List<string> PeriodList { get; }
+
+        public TimeSpan StartTime
+        {
+            get
+            {
+               return  Options.notificationStartTime;
+            }
+            set
+            {
+                if(Options.notificationStartTime != value)
+                {
+                    Options.notificationStartTime = value;
+                    Options.SaveOptions();
+                }
+            }
+        }
+        public TimeSpan EndTime
+        {
+            get
+            {
+                return Options.notificationEndTime;
+            }
+            set
+            {
+                if (Options.notificationEndTime != value)
+                {
+                    Options.notificationEndTime = value;
+                    Options.SaveOptions();
+                }
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return Options.GetNotificationType();
+            }
+            set
+            {
+                if (Options.GetNotificationType() != value && value != null)
+                {
+                    Options.SetNotificationType(value);
+                    Options.SaveOptions();
+                }
+            }
+        }
+
+        public string Period
+        {
+            get
+            {
+                return PeriodList[Options.notificationPeriod];
+            }
+            set
+            {
+                var result = PeriodList.FindIndex(x=> x==value);
+                if (Options.notificationPeriod != result)
+                {
+                    Options.notificationPeriod = result;
+                    Options.SaveOptions();
+                }
+            }
+        }
+
+
+    }
+}
