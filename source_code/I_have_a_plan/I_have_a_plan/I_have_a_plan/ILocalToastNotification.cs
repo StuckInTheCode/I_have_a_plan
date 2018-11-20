222fb23dc360e4f83c8557050dc9b20a95868b35
StuckInTheCode <gala-1999@tut.by>
2018-11-21 01:18:13 +0300

    Add local toast notification implementation

---
+++ source_code/I_have_a_plan/I_have_a_plan/I_have_a_plan/ILocalToastNotification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_have_a_plan
+{
+    public interface ILocalToastNotification
+    {
+        void ShowScheduledMessage(string name, string message, int id, bool silent, DateTime time);
+        void ShowMessage(string name, string message, int id = 0);
+        void CancelMessage(int id);
+    }
+}
